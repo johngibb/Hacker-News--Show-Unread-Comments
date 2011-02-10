c016cada2c350cb8652f0055b459f066ed721cf4
John Gibb <johngibb@gmail.com>
2011-02-09 18:14:07 -0800

    turn read links grep, and new ones should have a *NEW

--- show_read.js
+++ show_read.js
@@ -1,1 +1,19 @@
-console.log("show read comments loaded")
+var links = $('a[href^=item?id=]'); 
+
+function getId(link){
+  return link.attr('href').replace(/.*=/, '');
+}
+
+var readLinks = { 1778913 :0, 1778742: 0 };
+
+links.each(function(){
+  var link = $(this);
+  var block = $(this).closest('td.default');
+  var id = getId(link);
+  if (id in readLinks) {
+    block.find("*").css('color', 'grey')
+  }
+  else {
+    link.after( " *NEW");    
+  }
+})
