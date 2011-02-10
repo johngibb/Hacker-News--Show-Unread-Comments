b3ba97ae07b0ae8859b6101d3291c5b1aefe482c
John Gibb <johngibb@gmail.com>
2011-02-09 20:19:38 -0800

    remove useless function call

--- db.js
+++ db.js
@@ -1,23 +1,19 @@
 var db = openDatabase('ReadLinks', '1.0', 'readlinks', 2 * 1024 * 1024);
 
-function GetDB(){
-  return db;
-}
-
 function EnsureTable(){
-  GetDB().transaction(function (tx) {
+  db.transaction(function (tx) {
     tx.executeSql('CREATE TABLE IF NOT EXISTS ReadLinks (linkId int unique)');
   })
 }
 
 function AddLinkId(linkId){
-    GetDB().transaction(function (tx) {
+    db.transaction(function (tx) {
       tx.executeSql('INSERT INTO ReadLinks (linkId) VALUES (?)', [linkId]);
     });
 }
 
 function CheckLinkId(linkId, callback){
-    GetDB().transaction(function (tx) {
+    db.transaction(function (tx) {
       tx.executeSql('select * from ReadLinks where linkId = ?', [linkId], function(tx, results){
         callback(results.rows.length > 0);
       });
