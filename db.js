b413189612d2b0f22973807840c63c1359fcf845
John Gibb <johngibb@gmail.com>
2011-02-09 21:09:46 -0800

    no point in caching anymore since it only loads once

--- db.js
+++ db.js
@@ -7,23 +7,18 @@ function EnsureTable(){
 }
 
 function AddLinkId(linkId){
-    db.transaction(function (tx) {
-      tx.executeSql('INSERT INTO ReadLinks (linkId) VALUES (?)', [linkId]);
-    });
+  db.transaction(function (tx) {
+    tx.executeSql('INSERT INTO ReadLinks (linkId) VALUES (?)', [linkId]);
+  });
 }
 
-var linkIdCache = {};
 function CheckLinkId(linkId, callback){
-  if ( linkId in linkIdCache ){
-    callback(true);
-  }
   db.transaction(function (tx) {
-      tx.executeSql('select * from ReadLinks where linkId = ?', [linkId], function(tx, results){
-        var isRead = (results.rows.length > 0);
-        if (isRead) linkIdCache[linkId] = true;
-        callback(isRead);
-      });
+    tx.executeSql('select * from ReadLinks where linkId = ?', [linkId], function(tx, results){
+      var isRead = (results.rows.length > 0);
+      callback(isRead);
     });
+  });
 }
 
 EnsureTable();
